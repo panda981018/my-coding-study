d4e04aeb0affeeb0c4e66811a85f18e7c02a4518
jiwon <panda981018@gmail.com>
2023-12-11 16:13:28 +0900

    12/11 스프링 핵심 원리-기본편 section 3 노트 및 예제 수정 README 수정

--- lecture/스프링핵심원리_기본편/example_project/core/src/main/java/hello/core/OrderApp.java
+++ lecture/스프링핵심원리_기본편/example_project/core/src/main/java/hello/core/OrderApp.java
@@ -3,22 +3,35 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
+        /* 1트
         MemberService memberService = new MemberServiceImpl();
         OrderService orderService = new OrderServiceImpl();
+        */
+
+        /* 2트
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+        */
+
+        // 3트
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = context.getBean("memberService", MemberService.class);
+        OrderService orderService = context.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
     }
