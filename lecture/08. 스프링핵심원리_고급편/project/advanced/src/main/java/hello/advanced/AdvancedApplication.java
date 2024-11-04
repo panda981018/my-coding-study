19b9d37cf4b4338174d8d240ec2324200a581ae0
jiwon <panda981018@gmail.com>
2024-11-04 10:47:18 +0900

    11/04 스프링고급편 section3, 4 코드 추가(ThreadLocal, 템플릿메서드패턴 코드 추가)

--- lecture/08. 스프링핵심원리_고급편/project/advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ lecture/08. 스프링핵심원리_고급편/project/advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -3,10 +3,12 @@ package hello.advanced;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class AdvancedApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
