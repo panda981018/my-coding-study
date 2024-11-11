d9d01c9f5cf47e5a78add9c41731d33f4210dc24
jiwon <panda981018@gmail.com>
2024-11-11 17:59:01 +0900

    11/11 JPA 기본편 section2, 3추가 완료, section4 일부 추가

--- lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,16 +1,36 @@
 package hellojpa;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZZ");
+
+            System.out.println("==============");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
