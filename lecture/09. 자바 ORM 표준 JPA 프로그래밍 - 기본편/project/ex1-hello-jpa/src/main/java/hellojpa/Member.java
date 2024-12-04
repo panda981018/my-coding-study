4c060988eda8602c4ef7892bf02fdae0523103f5
jiwon <panda981018@gmail.com>
2024-12-04 13:50:24 +0900

    12/04 JPA 기본편 section6 ex1-hello-jpa N:M 관계 추가

--- lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -21,6 +24,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts;
 
     public Long getId() {
         return id;
