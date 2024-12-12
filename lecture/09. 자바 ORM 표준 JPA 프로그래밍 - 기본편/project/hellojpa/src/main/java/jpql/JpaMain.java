2f7cd42ee96c14240e943782ce3ff34a8ec841be
jiwon <panda981018@gmail.com>
2024-12-12 17:43:17 +0900

    12/12 JPA 기본편 section11 hellojpa 세팅

---
+++ lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/hellojpa/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,43 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(20);
+            em.persist(member);
+
+            Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                                    .setParameter("username", "member1")
+                                    .getSingleResult();
+            System.out.println("findMember = " + findMember.getUsername());
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2 : " + (m1 instanceof Member));
+        System.out.println("m1 == m2 : " + (m2 instanceof Member));
+    }
+}
