bbd7c7bfa296af77b364a5878a54b583ff833422
jiwon <panda981018@gmail.com>
2024-12-12 16:40:46 +0900

    12/12 JPA 기본편 section11 ex1-hello-jpa jpql 소개

--- lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ lecture/09. 자바 ORM 표준 JPA 프로그래밍 - 기본편/project/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,10 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import org.hibernate.Hibernate;
 
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
 
@@ -21,35 +19,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("======== START ===========");
-            // homeCity -> newCity
-//            Member findMember = em.find(Member.class, member.getId());
-//            Address oldAddress = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            List resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME FROM Member", Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
